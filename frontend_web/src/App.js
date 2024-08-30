65da8084eebee339b61754b12c05d987f97b03ae
shopie14 <sh.pinh14@gmail.com>
2024-08-30 07:19:10 +0700

    setting routes

--- frontend_web/src/App.js
+++ frontend_web/src/App.js
@@ -1,14 +1,49 @@
 import "./App.css";
 import Dashboard from "./modules/Dashboard";
 import Form from "./modules/Form";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = localStorage.getItem("user:token") != null || true;
+
+  if (!isLoggedIn) {
+    return <Navigate to={"/users/login"} />;
+  } else if (
+    isLoggedIn &&
+    ["/users/login", "/users/daftar"].includes(window.location.pathname)
+  ) {
+    return <Navigate to={"/"} />;
+  }
+  return children;
+};
 
 function App() {
   return (
     <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/users/login" element={<Form isLoginPage={true} />} />
-      <Route path="/users/daftar" element={<Form isLoginPage={false} />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users/login"
+        element={
+          <ProtectedRoute>
+            <Form isLoginPage={true} />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users/daftar"
+        element={
+          <ProtectedRoute>
+            <Form isLoginPage={false} />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
--- frontend_web/src/modules/Form/index.js
+++ frontend_web/src/modules/Form/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
+import { useNavigate } from "react-router-dom";
 
 const Form = ({ isLoginPage = true }) => {
   const [data, setData] = useState({
@@ -12,79 +13,102 @@ const Form = ({ isLoginPage = true }) => {
     kota: "",
   });
 
-  return (
-    <div className="bg-white h-[600px] w-[600px] shadow-lg rounded-lg flex flex-col items-center justify-center">
-      <div className="text-4xl font-extrabold">
-        Welcome 👋 {isLoginPage && "Back"}
-      </div>
-      <div className="text-2xl font-light mb-8">
-        {isLoginPage
-          ? "Silahkan login terlebih dahulu!"
-          : "Silahkan daftar terlebih dahulu!"}
-      </div>
+  const navigate = useNavigate();
 
-      <form
-        className="flex flex-col items-center w-full"
-        onSubmit={() => console.log("submitted")}
-      >
-        <Input
-          label="Username"
-          name="username"
-          placeholder="Masukan username"
-          className="mb-3"
-          value={data.username}
-          onChange={(e) => setData({ ...data, username: e.target.value })}
-        />
-        <Input
-          label="Password"
-          name="password"
-          placeholder="Masukan password"
-          className="mb-3"
-          value={data.password}
-          onChange={(e) => setData({ ...data, password: e.target.value })}
-        />
-        {!isLoginPage && (
-          <>
-            <Input
-              label="No WhatsApp"
-              name="number"
-              placeholder="Masukan no WhatsApp"
-              className="mb-3"
-              value={data.no_whatsApp}
-              onChange={(e) =>
-                setData({ ...data, no_whatsApp: e.target.value })
-              }
-            />
-            <Input
-              label="Kota"
-              name="city"
-              placeholder="Masukan kota"
-              className="mb-3"
-              value={data.kota}
-              onChange={(e) => setData({ ...data, kota: e.target.value })}
-            />
-          </>
-        )}
-        <Button
-          label={isLoginPage ? "Login" : "Daftar"}
-          className="w-3/6 mb-3 mt-7"
-          type="submit"
-        />
-      </form>
-      <div className="mt-4">
-        {isLoginPage ? (
-          <>
-            Belum punya akun?{" "}
-            <span className="text-primary cursor-pointer underline">
-              Daftar
-            </span>
-          </>
-        ) : (
-          <>
-            Sudah punya akun?{" "}
-            <span className="text-primary cursor-pointer underline">Login</span>
-          </>
-        )}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+    // Navigasi ke halaman berikutnya berdasarkan kondisi tertentu
+    if (isLoginPage) {
+      navigate("/dashboard");
+    } else {
+      navigate("/login");
+    }
+  };
+
+  return (
+    <div className="bg-primary-light h-screen flex items-center justify-center">
+      <div className="bg-white h-[600px] w-[600px] shadow-lg rounded-lg flex flex-col items-center justify-center">
+        <div className="text-4xl font-extrabold">
+          Welcome 👋 {isLoginPage && "Back"}
+        </div>
+        <div className="text-2xl font-light mb-8">
+          {isLoginPage
+            ? "Silahkan login terlebih dahulu!"
+            : "Silahkan daftar terlebih dahulu!"}
+        </div>
+
+        <form
+          className="flex flex-col items-center w-full"
+          onSubmit={handleSubmit}
+        >
+          <Input
+            label="Username"
+            name="username"
+            placeholder="Masukan username"
+            className="mb-3 w-[50%]"
+            value={data.username}
+            onChange={(e) => setData({ ...data, username: e.target.value })}
+          />
+          <Input
+            label="Password"
+            name="password"
+            placeholder="Masukan password"
+            className="mb-3 w-[50%]"
+            value={data.password}
+            onChange={(e) => setData({ ...data, password: e.target.value })}
+          />
+          {!isLoginPage && (
+            <>
+              <Input
+                label="No WhatsApp"
+                name="number"
+                placeholder="Masukan no WhatsApp"
+                className="mb-3 w-[50%]"
+                value={data.no_whatsApp}
+                onChange={(e) =>
+                  setData({ ...data, no_whatsApp: e.target.value })
+                }
+              />
+              <Input
+                label="Kota"
+                name="city"
+                placeholder="Masukan kota"
+                className="mb-3 w-[50%]"
+                value={data.kota}
+                onChange={(e) => setData({ ...data, kota: e.target.value })}
+              />
+            </>
+          )}
+          <Button
+            label={isLoginPage ? "Login" : "Daftar"}
+            className="mb-3 w-[50%] mt-7"
+            type="submit"
+          />
+        </form>
+        <div className="mt-4">
+          {isLoginPage ? (
+            <>
+              Belum punya akun?{" "}
+              <span
+                className="text-primary cursor-pointer underline"
+                onClick={() => navigate("/users/daftar")}
+              >
+                Daftar
+              </span>
+            </>
+          ) : (
+            <>
+              Sudah punya akun?{" "}
+              <span
+                className="text-primary cursor-pointer underline"
+                onClick={() => navigate("/users/login")}
+              >
+                Login
+              </span>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
